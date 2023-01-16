0c9a21fe80a25c80cde3b24ef50db4c956a9a77d
Quander <sanderkosterman@gmail.com>
2023-01-16 19:41:15 +0100

    Update main.py
    more tweaks, very minor

--- src/main.py
+++ src/main.py
@@ -13,6 +13,8 @@ background_colour = '#1b2838' #steam dark blue
 figure_colour = '#2a475e' #steam blue
 backboard_colour = '#232323' #steam dark grey
 highlight_colour = '#66c0f4' #steam light blue
+graph_frame_padx = 20
+graph_frame_pady = 20
 
 
 # function to open the info windows when clicking on charts
@@ -35,7 +37,7 @@ def on_leave(fig, canvas, event):
 # Function to create a pie chart
 def create_pie_chart(title, axis, data):
     plt.rcParams['text.color'] = text_colour
-    fig = Figure(facecolor=figure_colour)
+    fig = Figure(facecolor=figure_colour, figsize=(5,4))
     ax = fig.add_subplot(111)
     ax.set_facecolor(backboard_colour)
     ax.set_title(title)
@@ -49,7 +51,7 @@ def create_pie_chart(title, axis, data):
 # Function to create a bar chart
 def create_bar_chart(title, axis, data):
     plt.rcParams['text.color'] = text_colour
-    fig = Figure(facecolor=figure_colour)
+    fig = Figure(facecolor=figure_colour, figsize=(5,4))
     ax = fig.add_subplot(111)
     ax.set_facecolor(backboard_colour)
     x_values = axis
@@ -62,7 +64,7 @@ def create_bar_chart(title, axis, data):
 # Function to create a progress bar
 def create_progress_bar(title,  percentage):
     plt.rcParams['text.color'] = text_colour
-    fig = Figure(facecolor=figure_colour)
+    fig = Figure(facecolor=figure_colour, figsize=(5,4))
     ax = fig.add_subplot(513)
     ax.set_facecolor(backboard_colour)
     ax.set_xlim([0, 100])
@@ -77,33 +79,38 @@ def create_progress_bar(title,  percentage):
 def initiate_pie_chart(title, plot_data, axis_titles, frame):
     fig = create_pie_chart(title, axis_titles, plot_data)
     canvas = FigureCanvasTkAgg(fig, master=frame)
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+    canvas.get_tk_widget().bind("<Configure>", lambda event: canvas.draw())
     fig.canvas.mpl_connect('button_press_event', functools.partial(on_click, fig, title))
     canvas.get_tk_widget().bind("<Enter>", functools.partial(on_enter, fig, canvas))
     canvas.get_tk_widget().bind("<Leave>", functools.partial(on_leave, fig, canvas))
     canvas.draw()
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
 
 
 # send the data to the barchart creator and configure the returned chart
 def initiate_bar_chart(title, plot_data, axis_titles, frame):
     fig = create_bar_chart(title, axis_titles, plot_data)
     canvas = FigureCanvasTkAgg(fig, master=frame)
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+    canvas.get_tk_widget().bind("<Configure>", lambda event: canvas.draw())
     fig.canvas.mpl_connect('button_press_event', functools.partial(on_click, fig, title))
     canvas.get_tk_widget().bind("<Enter>", functools.partial(on_enter, fig, canvas))
     canvas.get_tk_widget().bind("<Leave>", functools.partial(on_leave, fig, canvas))
     canvas.draw()
-    canvas.get_tk_widget().pack(side=tk.LEFT)
+
 
 
 # send the data to the progress bar creator and configure the returned bar
 def initiate_progress_bar(title, percentage, frame):
     fig = create_progress_bar(title, percentage)
     canvas = FigureCanvasTkAgg(fig, master=frame)
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+    canvas.get_tk_widget().bind("<Configure>", lambda event: canvas.draw())
     fig.canvas.mpl_connect('button_press_event', functools.partial(on_click, fig, title))
     canvas.get_tk_widget().bind("<Enter>", functools.partial(on_enter, fig, canvas))
     canvas.get_tk_widget().bind("<Leave>", functools.partial(on_leave, fig, canvas))
     canvas.draw()
-    canvas.get_tk_widget().pack(side=tk.LEFT)
 
 
 # make the gui frames and anything that does not change
@@ -113,59 +120,66 @@ def gui():
     root.columnconfigure(0, weight=1)
     root.rowconfigure(1, weight=1)
 
-    # Create a frame for the top bar with a title
+    # Create a frame for the top bar with a title and a profile button
     top_bar = tk.Frame(root, bg=backboard_colour, height=50)
-    top_bar.grid(row=0, column=1, columnspan=4, sticky="we")
+    top_bar.grid(row=0, column=1, columnspan=3, sticky="wse")
     top_bar.grid_columnconfigure(2, weight=1)
 
     title_label = tk.Label(top_bar, text="Steam Dashboard", font=("Arial", 16), fg="white", bg=backboard_colour, anchor=tk.CENTER)
     title_label.grid(row=0, column=2, pady=5)
 
-    # Create a bar next to the top bar with 'Friends'
-    friend_bar = tk.Frame(root, bg=backboard_colour, height=50)
-    friend_bar.grid(row=0, column=0, columnspan=1, sticky="we")
-    friend_bar.grid_columnconfigure(2, weight=1)
+    profile_bar = tk.Frame(root, bg=backboard_colour, height=50)
+    profile_bar.grid(row=0, column=0, columnspan=1, sticky="new")
+    profile_bar.grid_columnconfigure(2, weight=1)
 
-    friend_label = tk.Label(friend_bar, text="Friends", font=("Arial", 16), fg="white", bg=backboard_colour, anchor=tk.CENTER)
-    friend_label.grid(row=0, column=2, pady=5)
+    profile_label = tk.Label(profile_bar, text="Profile", font=("Arial", 16), fg="white", bg=backboard_colour, anchor=tk.CENTER)
+    profile_label.grid(row=0, column=0, pady=5)
 
     # Create a frame for the listbox
     listbox_frame = tk.Frame(root, bg=backboard_colour)
-    listbox_frame.grid(row=1, column=0, rowspan=3, padx=0, pady=0, sticky='ns')
+    listbox_frame.grid(row=2, column=0, rowspan=3, padx=0, pady=0, sticky='news')
 
     # Create a filled listbox
-    listbox = tk.Listbox(listbox_frame, bg=backboard_colour, fg='white', selectmode='browse', font=('Helvetica', 20))
-    listbox.config(highlightthickness=0)
-    listbox.pack(side='left', fill='y')
+    friends_list = tk.Listbox(listbox_frame, bg=backboard_colour, fg='white', selectmode='browse', font=('Helvetica', 20))
+    friends_list.config(highlightthickness=0)
+    friends_list.pack(side='left', fill='y')
+    friends_list.insert(0, "Friends")
     for i in range(10):
-        listbox.insert(tk.END, f'Friend {i + 1}')
+        friends_list.insert(tk.END, f'Friend {i + 1}')
     frames()
 
 
 # here the frames are called and given data
 def frames():
     frame1 = tk.Frame(root)
-    frame1.grid(row=1, column=1, padx=20, pady=20)
+    frame1.grid(row=2, column=1, padx=graph_frame_padx, pady=graph_frame_pady)
+    frame1.grid_propagate(False)
     initiate_pie_chart("pie chart demo 1", (1, 2, 3, 4, 5), ('A', 'B', 'C', 'D', 'E'), frame1)
 
     frame2 = tk.Frame(root)
-    frame2.grid(row=1, column=2, padx=20, pady=20)
+    frame2.grid(row=2, column=2, padx=graph_frame_padx, pady=graph_frame_pady)
+    frame2.grid_propagate(False)
+
     initiate_bar_chart("bar chart demo 1", (1, 2, 3, 4, 5), ('A', 'B', 'C', 'D', 'E'), frame2)
 
     frame3 = tk.Frame(root)
-    frame3.grid(row=1, column=3, padx=20, pady=20)
+    frame3.grid(row=2, column=3, padx=graph_frame_padx, pady=graph_frame_pady)
+    frame3.grid_propagate(False)
     initiate_bar_chart("bar chart demo 2", (3, 2, 3, 4, 3), ('A', 'B', 'C', 'D', 'E'), frame3)
 
     frame4 = tk.Frame(root)
-    frame4.grid(row=2, column=1, padx=20, pady=20)
+    frame4.grid(row=3, column=1, padx=graph_frame_padx, pady=graph_frame_pady)
+    frame4.grid_propagate(False)
     initiate_pie_chart("pie chart demo 2", (4, 2, 4, 4, 5), ('A', 'B', 'C', 'D', 'E'), frame4)
 
     frame5 = tk.Frame(root)
-    frame5.grid(row=2, column=2, padx=20, pady=20)
+    frame5.grid(row=3, column=2, padx=graph_frame_padx, pady=graph_frame_pady)
+    frame5.grid_propagate(False)
     initiate_pie_chart("pie chart demo 3", (1, 2, 5, 4, 5), ('A', 'B', 'C', 'D', 'E'), frame5)
 
     frame6 = tk.Frame(root)
-    frame6.grid(row=2, column=3, padx=20, pady=20)
+    frame6.grid(row=3, column=3, padx=graph_frame_padx, pady=graph_frame_pady)
+    frame6.grid_propagate(False)
     initiate_progress_bar("progress bar demo", 23, frame6)
 
 
